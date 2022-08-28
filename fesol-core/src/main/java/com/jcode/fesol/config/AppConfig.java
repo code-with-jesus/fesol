64d2c0eeee2215e2e75e0e349593e41d42c93135
code-with-jesus <jdyanquen@gmail.com>
2022-08-27 22:19:56 -0500

    Updated configuration to XML

--- fesol-core/src/main/java/com/jcode/fesol/config/AppConfig.java
+++ fesol-core/src/main/java/com/jcode/fesol/config/AppConfig.java
@@ -2,10 +2,8 @@ package com.jcode.fesol.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "com.jcode.fesol")
 public class AppConfig {
 
